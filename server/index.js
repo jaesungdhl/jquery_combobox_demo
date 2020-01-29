c9c64f789b878b1719fbcc7941ce9bf87bdb9d7c
Jaesung Lee <jaesungdhl@gmail.com>
2020-01-28 19:26:56 -0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import faker from "faker";
+import searchEngine from "search-engine";
+import localtunnel from "localtunnel";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req,res) => res.render(`${__dirname}/views/index`));
+
+app.get("/api/randomlist", (req,res) => {
+    let randomJSON = {
+        listCount: 300,
+        list: [],
+    };
+    let maximumCount = 300;
+    let trial = 0;
+    let itemCount = 0;
+    while(itemCount < 50) {
+        if(!Object.keys(req.query).length) {
+            randomJSON.list.push({
+                item: `${faker.name.firstName()}`
+            });
+            itemCount++;
+        } else {
+            console.log("current trial count: " + trial + req.query.query);
+            if(trial == maximumCount) break;
+            let currentItem = faker.name.firstName();
+            if( new RegExp(`^${req.query.query}`, "i").test(currentItem) ) {
+                randomJSON.list.push({
+                    item: `${currentItem}`
+                });
+                itemCount++;
+            }
+            trial++;
+        }
+    }
+    console.log(randomJSON);
+    res.json(randomJSON);
+})
+app.listen(3000, console.log("your application has started to listen on port 3000"));
